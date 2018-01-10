6f0710b058a110fde13cedb7da3d6f81fdc1df3e
Andy Tian (Beyondsoft Corporation) <v-atian@microsoft.com>
2018-01-09 23:54:48 -0800

    Add project files.

---
+++ ChakraTopSitesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChakraTopSitesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChakraTopSitesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("463be8a5-2542-4e0b-9e5c-10646577d005")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ChakraTopSitesTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace ChakraTopSitesTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            EdgeDriver driver = new EdgeDriver();
+
+            driver.Url = "https://www.microsoft.com/";
+
+            driver.Quit();
+        }
+    }
+}
